e8bcf48a6ea33e5f0004ca5359cbbd0e9ed8b648
Guillaume <guillaume@wechantloup.com>
2022-04-14 10:54:46 +0200

    Split files

--- src/main.c
+++ src/main.c
@@ -13,7 +13,6 @@ static void handleInput();
 static void joyEvent(u16 joy, u16 changed, u16 state);
 static void moveP1(s16 increment);
 static void manageBall();
-static void splash();
 static void game();
 
 bool paused;
@@ -43,61 +42,6 @@ int main(u16 hard)
     game();
 
     return 0;
-}
-
-static void splash()
-{
-    s16 wait = 0;
-    u16 palette[64];
-    u16 ind;
-
-    u16 bgBaseTileIndex[2];
-    Map *bgb;
-
-    VDP_setScreenWidth320();
-    VDP_setPaletteColors(0, (u16 *)palette_black, 64);
-
-    // load background tilesets in VRAM
-    ind = TILE_USERINDEX;
-    bgBaseTileIndex[0] = ind;
-    VDP_loadTileSet(&bgb_tileset, ind, DMA);
-    ind += bgb_tileset.numTile;
-
-    bgb = MAP_create(&bgb_map, BG_B, TILE_ATTR_FULL(0, FALSE, FALSE, FALSE, bgBaseTileIndex[0]));
-    s16 camX = 0;
-    MAP_scrollTo(bgb, camX, 0);
-
-    memcpy(&palette[0], palette_splash.data, 16 * 2);
-    memcpy(&palette[16], palette_splash.data, 16 * 2);
-
-    VDP_setTextPalette(1);
-    VDP_drawText("Wechant Loup presents", 9, 22);
-    VDP_setHorizontalScroll(BG_A, 4);
-
-    PAL_fadeIn(0, (1 * 16) - 1, palette, 20, FALSE);
-
-    while (camX < 320)
-    {
-        camX += 4;
-        MAP_scrollTo(bgb, camX, 0);
-        SYS_doVBlankProcess();
-    }
-    PAL_fadeIn(16, (2 * 16) - 1, palette, 20, FALSE);
-    while (wait < 180)
-    {
-        wait++;
-        SYS_doVBlankProcess();
-    }
-    PAL_fadeOut(16, (2 * 16) - 1, 20, FALSE);
-    while (camX < 640)
-    {
-        camX += 4;
-        MAP_scrollTo(bgb, camX, 0);
-        SYS_doVBlankProcess();
-    }
-    VDP_clearText(9, 22, 21);
-
-    MEM_free(bgb);
 }
 
 void game()
---
+++ src/splash.h
@@ -0,0 +1,8 @@
+#ifndef _SPLASH_H_
+#define _SPLASH_H_
+
+#include <genesis.h>
+
+extern void splash();
+
+#endif // _SPLASH_H_
